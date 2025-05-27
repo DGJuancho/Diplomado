49d493b64d26817afe0807a044f2008172791bf6
DGJuancho <juancho.designs@icloud.com>
2025-05-27 14:45:13 -0400

    refactor: moviendo HolaMundo.java a src/

--- java/src/HolaMundo.java
+++ java/src/HolaMundo.java
@@ -1,12 +1,12 @@
 /*
-Este es un ejemplo de programa de Java
-    Debemos llamar al archivo HolaMundo.java
+    Este es un ejemplo de programa en Java
+        Debemos llamar al archivo HolaMundo.java
  */
 
 public class HolaMundo {
+    // Un programa en Java necesita un método main() por el que empezar
 
-        //Un programa en java necesita un método main() por el que empezar
     public static void main(String args[]) {
-        System.out.println("Hola Mundo!"); //Esta línea muestra por pantalla la frase Hola Mundo!
+        System.out.println("Hola Mundo"); // Esta línea muestra por pantalla la frase "Hola Mundo"
     }
 }
