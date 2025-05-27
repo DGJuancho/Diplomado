d80dd8acef586dc60c4cfb7b8d51ed64a3682ed2
DGJuancho <juancho.designs@icloud.com>
2025-05-27 16:01:50 -0400

    fix: eliminando archivos duplicados en carpeta python/

--- python/07_palindromo.py
+++
@@ -1,9 +0,0 @@
-"""Hacer un programa que reciba una string (cadena de caracteres) y detecte si es palindromo. (que se pueda leer igual de izquierda a derecha y viceversa).
-"""
-
-palabra = input("Ingrese una palabra: ")
-palabra_minus = palabra.lower()
-palindromo = palabra_minus[::-1]
-
-if palabra_minus == palindromo:
-    print(f'La palabra "{palabra}" es palindromo')
