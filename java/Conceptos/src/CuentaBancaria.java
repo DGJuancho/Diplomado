c97575b41e3cd0515780f4f8608d24900acb1f26
DGJuancho [OFFICE] <juancho.designs@icloud.com>
2025-09-12 12:59:20 -0400

    feat✨: Introducción a los métodos

--- java/Conceptos/src/CuentaBancaria.java
+++ java/Conceptos/src/CuentaBancaria.java
@@ -2,4 +2,16 @@ public class CuentaBancaria {
     String titular;
     String tipoDeCuenta;
     double saldo;
+
+    void ingresarDinero(double cantidad) {
+        saldo += cantidad;
+    }
+
+    void sacarDinero(double cantidad) {
+        saldo -= cantidad;
+    }
+
+    void cambiarTipoDeCuenta(String nuevoTipo) {
+        tipoDeCuenta = nuevoTipo;
+    }
 }
--- java/Conceptos/src/Main.java
+++ java/Conceptos/src/Main.java
@@ -5,6 +5,13 @@ public class Main {
         cuentaDeJuan.tipoDeCuenta = "nómina";
         cuentaDeJuan.saldo = 23500;
 
-        System.out.println("Cuále es el saldo disponible de la Cuenta de Juan?\n$ " + cuentaDeJuan.saldo);
+        cuentaDeJuan.sacarDinero(500);
+        System.out.println("Ahora la cuenta de Juan tiene " + cuentaDeJuan.saldo);
+
+        cuentaDeJuan.ingresarDinero(1000);
+        System.out.println("Ahora,al ingresar dinero,  la cuenta de Juan tiene " + cuentaDeJuan.saldo);
+
+        cuentaDeJuan.cambiarTipoDeCuenta("ahorro");
+        System.out.println("la cuenta de Juan ahora es: " + cuentaDeJuan.tipoDeCuenta);
     }
 }
